7d5775e19677d9713f13a880e88ab1bbbc137df4
Sibints2000 <sibints2000@gmail.com>
2021-08-14 22:04:26 +0530

    Sixth Commit

--- main.py
+++ main.py
@@ -30,5 +30,9 @@ resources = {
     "coffee": 100,
 }
 
-while True:
+is_on =  True
+
+while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
