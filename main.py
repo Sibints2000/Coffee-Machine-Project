55351d3b1653ea2dd3a672806443e803f09adecc
Sibints2000 <sibints2000@gmail.com>
2021-08-14 22:29:08 +0530

    Eleventh Commit

--- main.py
+++ main.py
@@ -32,11 +32,22 @@ resources = {
 }
 
 def is_resource_sufficient(order_ingredients):
+    """Returns True when can be made, and False if ingredients are insufficient."""
     for item in order_ingredients:
         if order_ingredients[item] >= resources[item]:
             print(f"Sorry there is not enough {item}.")
             return False
     return True
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10
+    total += int(input("How many nickels?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
+
 
 is_on = True
 
@@ -52,5 +63,5 @@ while is_on:
     else:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
-
+            payment = process_coins()
 
