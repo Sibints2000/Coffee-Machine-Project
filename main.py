8f0e3f5a7fc4443fba79442058ece339073688ed
Sibints2000 <sibints2000@gmail.com>
2021-08-14 22:22:48 +0530

    Tenth Commit

--- main.py
+++ main.py
@@ -31,6 +31,13 @@ resources = {
     "coffee": 100,
 }
 
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
 is_on = True
 
 while is_on:
@@ -44,5 +51,6 @@ while is_on:
         print(f"Money: ${profit}")
     else:
         drink = MENU[choice]
-        print(drink)
+        if is_resource_sufficient(drink["ingredients"]):
+
 
