4c703aeff9d3c2f4e9be2f66ecc67aa25aea6cd7
Sibints2000 <sibints2000@gmail.com>
2021-08-14 22:52:17 +0530

    Fourteenth Commit

--- main.py
+++ main.py
@@ -63,6 +63,12 @@ def is_transaction_successful(money_received, drink_cost):
         return False
 
 
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}.☕ Enjoy!!")
+
 is_on = True
 
 while is_on:
@@ -78,4 +84,6 @@ while is_on:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
-            is_transaction_successful(payment, drink["cost"])
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
